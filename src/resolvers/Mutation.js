23413495ef087951e7020580948176bb077f3675
Sidhanth <sidhanth@hatchsquad.com>
2019-04-04 20:41:06 +0530

    some optimization on bidding part

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -7,24 +7,6 @@ function hello () {
   return 'Hello'
 }
 
-// async function bidForProject (parent, args, context, info) {
-//   const userId = getUser(context)
-//   var bid = await Bid.findOne({
-//     forProject: args.for
-//   })
-//   console.log(bid)
-//   if (args.placedFor > bid.highest) {
-//     bid.highest = args.placedFor
-//     bid.by = userId
-//   } else {
-//     throw new Error('Bid is lower than current value')
-//   }
-
-//   var obj = await bid.save()
-//
-//   return obj
-// }
-
 async function bidForProject (parent, args, context, info) {
   const userId = getUser(context)
   var bid = await Bid.findOneAndUpdate({
