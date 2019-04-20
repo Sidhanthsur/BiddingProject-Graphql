4af5ab209652b6e918307974354d553688ea6eab
Sidhanth <sidhanth@hatchsquad.com>
2019-04-20 21:58:00 +0530

    db config hiding

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose')
-var { value } = require('./localaddress')
-console.log(process.env.MONGODBADDRESS ? process.env.MONGODBADDRESS : value)
-var db = mongoose.connect(process.env.MONGODBADDRESS ? process.env.MONGODBADDRESS : value, {useNewUrlParser: true})
+const fs = require('fs')
+var add = ''
+try {
+  if (fs.existsSync('./localaddress.js')) {
+    var {value} = require('./localaddress')
+    add = value
+  }
+} catch (err) {
+  console.error(err)
+}
+
+console.log(process.env.MONGODBADDRESS ? process.env.MONGODBADDRESS : add)
+var db = mongoose.connect(process.env.MONGODBADDRESS ? process.env.MONGODBADDRESS : add, {useNewUrlParser: true})
 
 module.exports = {
   db
